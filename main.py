e9d7cf5a53c4e9e346ddda32d14d51f2b0acacbe
implaymo <goncalomorerasilva@gmail.com>
2024-04-27 19:23:26 +0100

    Bullet not moving. Needs fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame 
 from spaceship import SpaceShipImg
+from projectil import Bullet
 
 pygame.init()
 screen = pygame.display.set_mode((600,400))
@@ -7,19 +8,31 @@ clock = pygame.time.Clock()
 running = True
 pygame.display.set_caption("Space Invaders")
 spaceship = SpaceShipImg()
+bullet = Bullet(bullet_x_pos=spaceship.spaceship_x_pos + 20, bullet_y_pos= spaceship.spaceship_y_pos + 20)
+
+
+all_bullets = []
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print(all_bullets)
             running = False
     
+    key = pygame.key.get_pressed()
     screen.fill("purple")
 
     screen.blit(spaceship.spaceship_resized, (spaceship.spaceship_x_pos, spaceship.spaceship_y_pos))
+    spaceship.move_spaceship(key=key)
 
-    spaceship.move_spaceship(screen=screen)
+    if key[pygame.K_SPACE]:
+        bullet.draw_bullet(screen=screen)
+        bullet.update_bullet()
+        
+    if key[pygame.K_UP]:
+        all_bullets.clear()
+    
     pygame.display.update()
-
     pygame.display.flip()
 
     clock.tick(60)
--- projectil.py
+++ projectil.py
@@ -1,8 +1,16 @@
 import pygame
 
 class Bullet():
-    def __init__(self,screen, bullet_x_pos, bullet_y_pos) -> None:
+    def __init__(self, bullet_x_pos, bullet_y_pos) -> None:
         self.direction = -1
+        self.bullet_x_pos = bullet_x_pos
+        self.bullet_y_pos = bullet_y_pos
         self.radius = 5
-        self.bullet = pygame.draw.circle(screen, color="black", center=[bullet_x_pos, bullet_y_pos], radius=self.radius)
-        self.vel = 8 * self.direction
+        self.color = "black"
+        self.vel = self.direction
+    
+    def draw_bullet(self, screen):
+        self.bullet = pygame.draw.circle(screen, self.color, center=[self.bullet_x_pos, self.bullet_y_pos ], radius=self.radius)
+
+    def update_bullet(self):
+        self.bullet_y_pos *= self.vel
