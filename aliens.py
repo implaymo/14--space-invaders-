467548e2e46500bbee526871a6ee5adac0eb230f
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-30 12:49:14 +0100

    Changed variable names for better readability.

--- aliens.py
+++ aliens.py
@@ -7,13 +7,13 @@ class AlienImg():
         self.alien_img = pygame.image.load(self.image_path).convert_alpha()
         self.alien_resized = pygame.transform.scale(self.alien_img, (20, 20))
 
-        self.alien_x_pos = 30
+        self.alien_x_pos = None
         self.alien_y_pos = 50
         self.all_aliens = []
         self.number_rows = 5
         self.total_alien_per_row = 3
-        self.row_gap_between_aliens = 20
-        self.col_gap_between_aliens = 30
+        self.row_gap_between_aliens =20
+        self.next_alien_x_pos = 30
         
     def store_aliens(self):
         for row in range(self.number_rows):
@@ -21,7 +21,7 @@ class AlienImg():
             for i in range(self.total_alien_per_row):
                 new_alien = AlienImg()
                 next_row_pos = (row * self.row_gap_between_aliens)
-                new_alien.alien_x_pos = i * self.col_gap_between_aliens
+                new_alien.alien_x_pos = i * self.next_alien_x_pos
                 new_alien.alien_y_pos = self.alien_y_pos + next_row_pos
                 self.all_aliens[row].append(new_alien)
         
