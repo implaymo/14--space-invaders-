1928ba60670b58f34552e5cbbd1e1e01692c1615
implaymo <goncalomorerasilva@gmail.com>
2024-05-09 15:54:01 +0100

    Add default font size in show_text method.

--- game_text.py
+++ game_text.py
@@ -7,7 +7,7 @@ class GameText():
         self.level = 1
         self.font = 'freesansbold.ttf'
         
-    def show_text(self, screen, x_pos, y_pos, font_size, message):
+    def show_text(self, screen, x_pos, y_pos, message, font_size=15):
         font = pygame.font.Font(self.font, font_size)
         self.text = font.render(message, True, self.green, self.blue)
         self.text_rect = self.text.get_rect()
